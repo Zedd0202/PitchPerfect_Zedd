bb93df5561f95d6774b1b465d6acb1e49dcc2f02
Zedd0202 <ch41150@hanmail.net>
2017-01-09 14:07:29 +0900

    record, stop 활성화, 비활성화

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,13 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+
     @IBOutlet weak var recordButton: UIButton!
     
     @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,11 +26,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func recordingAudio(_ sender: Any) {
+   
+    @IBAction func recordAudio(_ sender: Any) {
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
 
     @IBAction func stopRecording(_ sender: Any) {
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+        recordingLabel.text = "Tab to Record"
     }
 }
 
